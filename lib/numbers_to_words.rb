8047553869c1b14a101bbb881a3e508d5d0e484b
Ronando Long & Aileen Cacayorin <student@epicodus.com>
2015-08-04 16:30:38 -0700

    can translate integer from 0 - 10

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,7 +1,9 @@
+require('pry')
+
 class Fixnum
 
   define_method(:numbers_to_words) do
-    translate_key = {
+    single_digit = {
       0 => "zero",
       1 => "one",
       2 => "two",
@@ -14,6 +16,31 @@ class Fixnum
       9 => "nine",
       10 => "ten"
     }
-    translate_key.fetch(self)
+
+    double_digit = {
+      20 => "twenty",
+      30 => "thirty",
+      40 => "forty",
+      50 => "fifty",
+      60 => "sixty",
+      70 => "seventy",
+      80 => "eighty",
+      90 => "ninety"
+    }
+
+  #  if self.<(20)
+#       single_digit.fetch(self)
+#     else
+#       double_digit.fetch(self)
+#     end
+#   end
+# end
+  new_number = nil
+  number_array = self.to_s.split("")
+  number_array.each() do |number|
+    number = number.to_i
+    new_number = single_digit.fetch(number)
+  end
+    new_number
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -1,8 +1,14 @@
 require('rspec')
+require('pry')
 require('numbers_to_words')
 
 describe ("Fixnum#numbers_to_words") do
   it("translates fixnum from 0 to 9 to its string name") do
     expect(5.numbers_to_words()).to(eq("five"))
   end
+
+  # it("translates fixnum tens input") do
+  #   expect(20.numbers_to_words()).to(eq("twenty"))
+  # end
+
 end
